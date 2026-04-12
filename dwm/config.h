0d93f88b1314344b6a546dadea2c9ddb95e95036
multirepository <143484247+multirepository@users.noreply.github.com>
2026-04-12 22:52:19 +0300

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -16,11 +16,11 @@ static const char *fonts[]          = {
 	"FontAwesome:size=18"
 };
 static const char dmenufont[]       = "monospace:size=18";
-static const char col_gray1[]       = "#1e1e2e";
-static const char col_gray2[]       = "#313244";
-static const char col_gray3[]       = "#bac2de";
-static const char col_gray4[]       = "#cdd6f4";
-static const char col_cyan[]        = "#45475a";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#595856";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
