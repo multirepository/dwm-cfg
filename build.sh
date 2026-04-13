438439d38aba38355e8a10e7d33ed519d9919213
multirepository <143484247+multirepository@users.noreply.github.com>
2026-04-13 23:58:43 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ cd ~/dwm-cfg
 cd dwm && sudo make clean install
 cd ../dmenu && sudo make clean install
 cd ../slstatus && sudo make clean install
-cd .. && cp -rv ~/dwm-cfg/alacritty/ ~/.config/
+cp -rv ~/dwm-cfg/alacritty/ ~/.config/
